d9003f45e102ef537fd4528f3aa862186711359d
lacey <hunt3247@vandals.uidaho.edu>
2023-12-04 23:21:03 -0800

    combining lights and audio with server

--- server.py
+++ server.py
@@ -13,6 +13,9 @@ import time
 # "state" to the PI commandline.
 # From the computer, you would format the http request like the following:
 # http://<your pi's IP>:8000/?state=7
+
+#  MY PI IP: 192.168.117.114/24
+
 #
 # Apologies if this is review, but the :8000 specifies the port, and the ?state=7 is just a
 # "get" request which can be parsed out on the server side.
@@ -24,6 +27,9 @@ import time
 # "state_value" global allows us to be able to communicate from the server to the pygame loop running
 state_value = None
 running = True
+stop_event = threading.Event()
+audio_thread = None
+lights_thread = None
 
 # Set up GPIO
 GPIO.setmode(GPIO.BCM)
@@ -70,6 +76,73 @@ server_thread = threading.Thread(target=start_server)
 server_thread.daemon = True
 server_thread.start()
 
+
+
+def play_audio(file_path):
+    pygame.mixer.init()
+    pygame.mixer.music.load(file_path)
+
+    while not stop_event.is_set():
+        pygame.mixer.music.play()
+
+        # Wait for the audio to finish
+        while pygame.mixer.music.get_busy() and not stop_event.is_set():
+            pygame.time.Clock().tick(30)  # Adjust the tick rate as needed
+        pygame.mixer.quit()
+    print("Nativity Sound stopped.")
+
+def nativity_lights():
+    while not stop_event.is_set():
+        # star
+        GPIO.output(RELAY_PINS[7], GPIO.LOW)
+        # donkey
+        time.sleep(27.95)
+        GPIO.output(RELAY_PINS[6], GPIO.LOW)
+        # camel
+        time.sleep(11.12)
+        GPIO.output(RELAY_PINS[5], GPIO.LOW)
+        # lamb
+        time.sleep(4.66)
+        GPIO.output(RELAY_PINS[4], GPIO.LOW)
+        # cow
+        time.sleep(5.73)
+        GPIO.output(RELAY_PINS[3], GPIO.LOW)
+        # wisemen
+        time.sleep(5.5)
+        GPIO.output(RELAY_PINS[2], GPIO.LOW)
+        # joseph and mary
+        time.sleep(2.79)
+        GPIO.output(RELAY_PINS[1], GPIO.LOW)
+        # jesus
+        time.sleep(6.54)
+        GPIO.output(RELAY_PINS[0], GPIO.LOW)
+        time.sleep(125.71)
+    for pin in RELAY_PINS:
+            GPIO.output(pin, GPIO.HIGH)
+    print("Nativity Lights Stopped.")
+
+def start_nativity_threads():
+    global audio_thread, lights_thread, stop_event
+
+    # allow threads to run
+    stop_event.clear()
+
+    # initialize and start lights and audio threads
+    audio_thread = threading.Thread(target=play_audio, args=(audio_file,))
+    lights_thread = threading.Thread(target=nativity_lights)
+    audio_thread.start()
+    lights_thread.start()
+
+    return audio_thread,lights_thread
+
+def stop_nativity_threads():
+    global audio_thread, lights_thread, stop_event
+    stop_event.set()
+    if audio_thread is not None:
+        audio_thread.join()
+    if lights_thread is not None:
+        lights_thread.join()
+
 # Pygame loop - put all the normal PYGAME stuff in here
 audio_file = "ChristmasTrain2.mp3"  
 # clock = time.Clock()
@@ -89,37 +162,46 @@ try:
 
         # Check the state value
         if state_value is not None:
-            print("State value received:", state_value)
-            # Nativity Triggered
-            if state_value=='1':
-                print("Hello, ")
-                time.sleep(1)
-                print ("World")
-            elif state_value=='2':
-                pygame.mixer.init()
-                pygame.mixer.music.load(audio_file)
-                pygame.mixer.music.play()
+            # print("State value received:", state_value)
 
-                for pin in RELAY_PINS:
-                    GPIO.output(pin, GPIO.LOW)
-                    time.sleep(1)
-                    GPIO.output(pin, GPIO.HIGH)
-                    time.sleep(1)
-                time.sleep(pygame.mixer.music.get_length())  # Wait for audio to finish
-                pygame.mixer.quit()
-                # reset state_value
-            # turn off all pins
+            # Tunnels going
+            if state_value=='1' or state_value == '3':
+                print("State value received:", state_value)
+                stop_event.set()
+
+            # Nativity Triggered
+            elif state_value=='2':
+                print("State value received:", state_value)
+                stop_nativity_threads()
+                audio_thread,lights_thread=start_nativity_threads()
+                lights_thread.join()
+                audio_thread.join()
+                stop_event.set()
+
+            # Standby
             elif state_value=='4':
+                print("State value received:", state_value)
+                stop_event.set()
                 for pin in RELAY_PINS:
                     GPIO.output(pin,GPIO.HIGH)
+                # turn on star
+                GPIO.output(RELAY_PINS[7],GPIO.LOW)
+            
+            # Reset state_value
             state_value = None
 
 
 except KeyboardInterrupt:
     print("Pygame loop interrupted")
+except Exception as e:
+    print(f"Unexpected error: {e}")
 finally:
     print("Cleaning up...")
+    # Signal lights and audio to stop
+    stop_event.set()
+    stop_nativity_threads()
     # Signal the server thread to stop
     running = False
     # Join the server thread here so we can exit it with another CTRL-C
     server_thread.join()
+    GPIO.cleanup()
